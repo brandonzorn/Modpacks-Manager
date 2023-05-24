4cae7b48422e5659068187497cdd6b8532ebcfc0
bzorn <v.klimenko.2137@gmail.com>
2023-05-24 20:54:27 +0300

    added any

--- modrinthmanager/consts/items/modrinth_items.py
+++ modrinthmanager/consts/items/modrinth_items.py
@@ -3,6 +3,7 @@ from modrinthmanager.consts.items.parser_items import ParserItems
 
 class ModrinthItems(ParserItems):
     LOADERS = [
+        {'value': None, 'name': 'Any'},
         {'value': 'forge', 'name': 'Forge'},
         {'value': 'fabric', 'name': 'Fabric'},
         {'value': 'quilt', 'name': 'Quilt'}
